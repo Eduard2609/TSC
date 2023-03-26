4a32f458cc7f066eedc442c4890e425efa74d20d
Eduard2609 <eduardolteanu@gmail.com>
2023-03-26 14:14:57 +0300

    Update tema

--- lab3/lab2/lab_dut/instr_register.sv
+++ lab3/lab2/lab_dut/instr_register.sv
@@ -23,26 +23,25 @@ import instr_register_pkg::*;  // user-defined types are defined in instr_regist
   instruction_t  iw_reg [0:31];  // an array of instruction_word structures
   result_t res;
 
-  // write to the register
-  always@(posedge clk, negedge reset_n)   // write into register
+  // Write to the register
+  always @(posedge clk, negedge reset_n) 
     if (!reset_n) begin
-      foreach (iw_reg[i])
-        iw_reg[i] = '{opc:ZERO,default:0};  // reset to all zeros
+      foreach (iw_reg[i]) 
+        iw_reg[i] = '{opc:ZERO, op_a:0, op_b:0, rezultat:0};
     end
     else if (load_en) begin
-    case(opcode)
-        ZERO: res = 0;
-        PASSA: res = operand_a;
-        PASSB: res = operand_b;
-        ADD:   res = operand_a + operand_b;
-        SUB:   res = operand_a - operand_b;
-        MULT:   res = operand_a * operand_b;
-        DIV:   res = operand_a / operand_b;
-        MOD:   res = operand_a % operand_b;
-        default: res =0;
-    endcase
-      iw_reg[write_pointer] = '{opcode, operand_a, operand_b, res};
-    end
+      case (opcode)
+        ADD: rezultat = operand_a + operand_b;
+        SUB: rezultat = operand_a - operand_b;
+        MULT: rezultat = operand_a * operand_b;
+        PASSA: rezultat = operand_a;
+        PASSB: rezultat = operand_b;
+        DIV: rezultat = operand_a / operand_b;
+        MOD: rezultat = operand_a % operand_b;
+		ZERO: rezultat = 0; 
+      endcase     
+      iw_reg[write_pointer] = '{opcode,operand_a,operand_b, rezultat};
+      end
 
 //  always@(*) begin
 //     unique case (opcode)
